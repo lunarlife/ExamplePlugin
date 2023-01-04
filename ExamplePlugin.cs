49384d63466eae31c6de96bfb4984652663a8718
LunarLife <ivan.andreychenko1502@gmail.com>
2023-01-04 15:40:22 +0100

    new example

--- ExamplePlugin.cs
+++ ExamplePlugin.cs
@@ -1,8 +1,18 @@
 ﻿using Networking.Packets;
+using UndefinedNetworking.Core;
 using UndefinedNetworking.Events.Mouse;
+using UndefinedNetworking.GameEngine;
+using UndefinedNetworking.GameEngine.Input;
+using UndefinedNetworking.GameEngine.Objects.Components;
+using UndefinedNetworking.GameEngine.Scenes;
+using UndefinedNetworking.GameEngine.UI;
+using UndefinedNetworking.GameEngine.UI.Components;
+using UndefinedNetworking.GameEngine.UI.Components.Mouse;
 using UndefinedNetworking.GameEngine.UI.Elements.Enums;
+using UndefinedNetworking.GameEngine.UI.Elements.Structs;
 using UndefinedServer;
 using UndefinedServer.Events.PlayerEvents;
+using UndefinedServer.GameEngine.Objects;
 using UndefinedServer.Plugins;
 using UndefinedServer.UI.Elements;
 using Utils;
@@ -33,9 +43,40 @@ public sealed class ExamplePlugin : Plugin
     [EventHandler]
     private void OnJoinPlayer(PlayerConnectedEvent e)
     {
-        var text = new Text(new Rect(100, 100, 100, 100), "server text", Color.Aqua, fontStyle: FontStyle.Strikethrough);
-        var view = e.Player.Open(text);
-        Undefined.Logger.Info($"{e.Player.Nickname} open text with id " + view.Identifier);
+        //var text = new Text(new Rect(100, 100, 100, 100), "server text", Color.Aqua, fontStyle: FontStyle.Strikethrough);
+        //var view = e.Player.Open(text);
+        e.Player.LoadScene(SceneType.XY);
+        var view = e.Player.ActiveScene.OpenView(new ViewParameters
+        {
+            OriginalRect = new Rect(0, 200, 100, 100),
+            Bind = new UIBind
+            { 
+                Side = Side.TopRight
+            }
+        });
+        var text = e.Player.ActiveScene.OpenView(new ViewParameters
+        {
+            OriginalRect = new Rect(0, 0, 100, 30),
+            Bind = new UIBind
+            { 
+                Side = Side.TopRight
+            },
+            Parent = view.Transform
+        }).AddComponent<TextComponent>();
+        text.Size = new FontSize(32);
+        text.Color = Color.Aqua;
+        text.Text = "server text";
+        Task.Run(async () =>
+        {
+            await Task.Delay(2000);
+            var scroll = view.AddComponent<ScrollComponent>();
+            scroll.VerticalScrollSpeed = 1;
+            scroll.ViewRect = new Rect(20, 10, 50, 50);
+        });
+        // Undefined.Logger.Info($"{e.Player.Nickname} open text with id " + text.Identifier);
+
+ 
+
     }
 
     [EventHandler]
